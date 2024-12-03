50965e3d5286db33267c33b3ccc9f9e1fdd7ceb6
Mathias Fridthjof <mathiasfridthjof@live.dk>
2024-12-03 20:58:45 +0100

    first commit

---
+++ src/main/java/com/example/mealguider/MealguiderApplication.java
@@ -0,0 +1,13 @@
+package com.example.mealguider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealguiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealguiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mealguider/MealguiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mealguider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealguiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
